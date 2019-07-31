1b8e0daea77221371a426953a7bc8dc93d9f9467
Alex Xie <alex.xie.micro@outlook.com>
2019-07-31 23:17:52 +0800

    updated

--- core/app/__init__.py
+++ core/app/__init__.py
@@ -6,14 +6,6 @@ from .main.controller.auth_controller import api as auth_ns
 
 blueprint = Blueprint('api', __name__)
 
-if os.environ.get('HTTPS'):
-    @property
-    def specs_url(self):
-        """Monkey patch for HTTPS"""
-        return url_for(self.endpoint('specs'), _external=True, _scheme='https')
-
-    Api.specs_url = specs_url
-
 api = Api(blueprint,
           title='FLASK RESTPLUS API BOILER-PLATE WITH JWT',
           version='1.0',
